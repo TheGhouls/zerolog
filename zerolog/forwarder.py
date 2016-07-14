b9582a38992fe62b7e3c295242f59b61b1428d37
karec <manu.valette@gmail.com>
2016-07-14 17:56:01 +0200

    update docstring for forwarder

--- zerolog/forwarder.py
+++ zerolog/forwarder.py
@@ -4,9 +4,9 @@ import zmq
 def start_forwarder(pub_port, receive_port, mon_port=None):
     """Start a zeromq proxy for forwarding messages from TCP socket to zmq PUB socket
 
-    :param str pub_port: port number to use for publishing messages to workers
-    :param str receive_port: port number to use for receiving messages
-    :pram str mon_port (optional): port to use for monitor socket
+    :param int pub_port: port number to use for publishing messages to workers
+    :param int receive_port: port number to use for receiving messages
+    :pram int mon_port (optional): port to use for monitor socket
     """
     context = zmq.Context()
 
