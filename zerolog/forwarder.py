a6c9241efde5e564296f385aa7be9e926eac797f
karec <manu.valette@gmail.com>
2016-07-16 16:19:57 +0200

    update forwarder with logging

--- zerolog/forwarder.py
+++ zerolog/forwarder.py
@@ -1,4 +1,8 @@
+import logging
+
 import zmq
+
+log = logging.getLogger(__name__)
 
 
 def start_forwarder(pub_port, receive_port, mon_port=None):
@@ -19,6 +23,12 @@ def start_forwarder(pub_port, receive_port, mon_port=None):
     if mon_port is not None:
         monitor = context.socket(zmq.PUB)
         monitor.bind("tcp://*:{}".format(mon_port))
+
+        log.info("Starting forwarder")
+        log.info("frontend: {}\tbackend: {}\tmonitor: {}".format(pub_port, receive_port, mon_port))
+
         zmq.proxy(frontend, backend, monitor)
     else:
+        log.info("Starting forwarder")
+        log.info("frontend: {}\tbackend: {}".format(pub_port, receive_port))
         zmq.proxy(frontend, backend)
